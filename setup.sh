c8123326b2ae23b796284ec9ff2ce88b220b9582
tanviryuma <tanvir.yuma@gmail.com>
2025-02-01 11:05:34 +0600

    update

--- setup.sh
+++ setup.sh
@@ -22,12 +22,45 @@ echo "Verifying Node.js installation..."
 sudo node -v
 sudo npm -v
 
+
+
 sudo npm i pm2 -g
 
 echo "Installing Nginx..."
 sudo apt install -y nginx
 
+cat <<EOL > default.conf
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                proxy_http_version 1.1;
+                proxy_cache_bypass $http_upgrade;
+
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection 'upgrade';
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+
+                proxy_pass http://localhost:3002;
+        }
+
+}
+EOL
+
+# Move and enable Nginx config
+sudo mv default.conf /etc/nginx/sites-available/default
+
+echo "Restarting Nginx..."
+sudo systemctl restart nginx
 
 # Install UFW
 echo "Installing UFW..."
